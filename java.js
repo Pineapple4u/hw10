f3059ba6a6f2a86ebe4acb58a4c8cbdf2dc8d133
Pineapple4u <165797715+Pineapple4u@users.noreply.github.com>
2024-10-18 18:06:57 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+var x = "AARNA";
+var z = x.toLowerCase();
+console.log(z);
+
+var q = "kapuria";
+var w = q.toUpperCase();
+console.log(w);
+
+var a="english, hindi, french ";
+
+var slc = a.slice(-7, -1);
+console.log(slc);
+
+var sbsg = a.substring(0, 7);
+console.log(sbsg);
+
+var str = a.substr(9, 5);
+console.log(str);
